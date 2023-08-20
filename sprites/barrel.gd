6b765206d9c406a83b1689428dd1e507d2318c0b
Stuart McHattie <stuart.mchattie@gmail.com>
2023-08-20 14:39:06 +0100

    Update the way barrels are generated

--- sprites/barrel.gd
+++ sprites/barrel.gd
@@ -1,27 +1,27 @@
 class_name Barrel
 extends CharacterBody2D
 
+enum Type { REGULAR = 0, EXPLOSIVE }
+
 const SPEED = 175.0
-enum BarrelType { REGULAR = 0, EXPLOSIVE }
+const TEXTURES = {
+	Type.REGULAR: preload("res://assets/objects/Regular Barrel.png"),
+	Type.EXPLOSIVE: preload("res://assets/objects/Explosive Barrel.png")
+}
 
 @export var direction := 1
 @export var explosive_chance := 0.1
 @export var tumble_chance := 0.3
 
-var barrel_type: BarrelType
+var type: Type
 
-@onready var sprites = $Sprite2D as Sprite2D
+@onready var sprite = $Sprite2D as Sprite2D
 @onready var anim_tree = $AnimationTree as AnimationTree
 @onready var state_machine = $StateMachine as StateMachine
 
 func _init():
-	barrel_type = BarrelType.REGULAR if randf() > explosive_chance else BarrelType.EXPLOSIVE
+	type = Type.REGULAR if randf() > explosive_chance else Type.EXPLOSIVE
 
 func _ready():
-	var texture_name = BarrelType.keys()[barrel_type].to_camel_case()
-	sprites.texture = load("res://assets/objects/" + texture_name + " Barrel.png")
-	
+	sprite.texture = TEXTURES[type]
 	anim_tree.active = true
-
-func set_pose(new_pose: StringName):
-	anim_tree[state_machine.anim_playback_path].travel(new_pose)
--- sprites/donkey_kong.gd
+++ sprites/donkey_kong.gd
@@ -1,21 +1,24 @@
 extends Sprite2D
 
-var barrel_scene := preload("res://sprites/Barrel.tscn")
+var barrel_scene := preload("res://sprites/barrel.tscn")
 var new_barrel: Barrel
 
-@onready var barrels_group = owner.get_node("Barrels")
-@onready var barrel_spawn_point = $BarrelSpawnPoint as Marker2D
-@onready var barrel_move_point = $BarrelMovePoint as Marker2D
+@export var barrels_group : Node
+@onready var anim_barrel = $Barrel as Sprite2D
+@onready var anim_player = $AnimationPlayer as AnimationPlayer
 
-func spawn_barrel():
+func throw_barrel():
 	new_barrel = barrel_scene.instantiate()
-	barrels_group.add_child(new_barrel)
-	new_barrel.position = barrels_group.to_local(barrel_spawn_point.global_position)
-	new_barrel.set_pose("Tumbling")
+	match new_barrel.type:
+		Barrel.Type.REGULAR:
+			anim_player.play("throw_regular_barrel")
+		Barrel.Type.EXPLOSIVE:
+			anim_player.play("throw_explosive_barrel")
 
-func move_barrel():
-	new_barrel.position = barrels_group.to_local(barrel_move_point.global_position)
-	new_barrel.set_pose("Rolling")
-	
 func launch_barrel():
+	barrels_group.add_child(new_barrel)
+	new_barrel.global_position = anim_barrel.global_position
 	new_barrel.state_machine.transition_to("Roll")
+
+func _on_timer_timeout():
+	throw_barrel()
