273e7ab7ed6694805ab4f8c01fa8000ea6cd8981
Stuart McHattie <stuart.mchattie@gmail.com>
2023-09-17 20:03:17 +0100

    Move the flameball path handling to oil barrel

--- sprites/flame_ball.gd
+++ sprites/flame_ball.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
-@export var leap_speed = 100
 @export var navigate_speed = 30
 @export var target_node: Node2D
 @export var target_max_distance = 100.0
 
-@onready var path_follow = $"Initial Path/Path Follow" as PathFollow2D
-@onready var animatable_body = $"Initial Path/Path Follow/AnimatableBody2D" as AnimatableBody2D
-@onready var animated_sprite = $"Initial Path/Path Follow/AnimatableBody2D/AnimatedSprite2D" as AnimatedSprite2D
-@onready var anim_player = $"Initial Path/Path Follow/AnimatableBody2D/AnimationPlayer" as AnimationPlayer
+@onready var animatable_body = $AnimatableBody2D as AnimatableBody2D
+@onready var animated_sprite = $AnimatableBody2D/AnimatedSprite2D as AnimatedSprite2D
+@onready var anim_player = $AnimatableBody2D/AnimationPlayer as AnimationPlayer
 @onready var navigation_agent = $NavigationAgent2D as NavigationAgent2D
 @onready var refresh_target_timer = $RefreshTargetTimer as Timer
 
@@ -25,13 +23,8 @@ var _state = FlameBallState.LEAP
 func _physics_process(delta):
 	match _state:
 		FlameBallState.LEAP:
-			path_follow.progress += leap_speed * delta
-			if path_follow.progress_ratio == 1:
-				z_index = 20
-				renew_target()
-				_state = FlameBallState.FOLLOW_PATH
+			animatable_body.move_and_collide(Vector2.ZERO)
 		FlameBallState.FOLLOW_PATH:
-			anim_player.play("bounce_right")
 			var next_dir = to_local(
 				navigation_agent.get_next_path_position()
 			).normalized()
@@ -46,6 +39,11 @@ func _physics_process(delta):
 			animatable_body.move_and_collide(Vector2.ZERO)
 			if navigation_agent.is_navigation_finished():
 				renew_target()
+
+func start_following_paths():
+	renew_target()
+	_state = FlameBallState.FOLLOW_PATH
+	animatable_body.position = Vector2.ZERO
 
 func renew_target():
 	navigation_agent.target_position = target_node.global_position + Vector2(
--- sprites/oil_barrel.gd
+++ sprites/oil_barrel.gd
@@ -1,13 +1,28 @@
 extends Sprite2D
 
 var flame_ball_scene = preload("res://sprites/flame_ball.tscn")
+var frozen = false
 
 @onready var anim_player = $AnimationPlayer as AnimationPlayer
 @onready var ignite_timer = $IgniteTimer as Timer
-@onready var flame_ball_spawn_location = $"Flame Ball Spawn Location" as Marker2D
+@onready var initial_path = $"Initial Path" as Path2D
 
 @export var flame_ball_parent: Node
 @export var flame_ball_target: Node2D
+@export var flame_ball_leap_speed = 100
+
+func _physics_process(delta):
+	if frozen:
+		return
+
+	for path_follow in initial_path.get_children():
+		if not path_follow is PathFollow2D:
+			continue
+
+		path_follow.progress += flame_ball_leap_speed * delta
+
+		if path_follow.progress_ratio == 1:
+			_release_flame_ball(path_follow)
 
 func ignite():
 	if anim_player.current_animation == "ignite":
@@ -33,10 +48,35 @@ func _on_collision_area_body_exited(body):
 		_create_flame_ball()
 		body.queue_free()
 
+func _new_path_follow():
+	var path_follow = PathFollow2D.new()
+	path_follow.z_index = -10
+	path_follow.rotates = false
+	path_follow.cubic_interp = false
+	path_follow.loop = false
+
+	return path_follow
+
 func _create_flame_ball():
-	# Create a new flame ball
-	var flame_ball = flame_ball_scene.instantiate() as Node2D
-	flame_ball.global_position = flame_ball_spawn_location.global_position
-	flame_ball.target_node = flame_ball_target
-	flame_ball_parent.add_child(flame_ball)
 	ignite()
+
+	# Create a new flame ball
+	var path_follow = _new_path_follow()
+	var flame_ball = flame_ball_scene.instantiate() as Node2D
+	flame_ball.target_node = flame_ball_target
+	path_follow.add_child(flame_ball)
+	initial_path.add_child(path_follow)
+
+func _release_flame_ball(path_follow: PathFollow2D):
+	# Move the flame ball to its navigational parent
+	var flame_ball = path_follow.get_child(0) as Node2D
+	var position = flame_ball.global_position
+	path_follow.remove_child(flame_ball)
+	flame_ball_parent.add_child(flame_ball)
+	flame_ball.global_position = position
+	flame_ball.start_following_paths()
+
+	path_follow.queue_free()
+
+func freeze(should_freeze: bool):
+	frozen = should_freeze
